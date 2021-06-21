9dca7f1086ed2199f606a56d644b089289b225cc
Mg138 <muguang138@gmail.com>
2021-06-21 19:38:50 +0800

    egg

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="html-index",
-    version="1.0.3",
+    version="1.0.4",
     author="Madeline Sparkle",
     author_email="muguang138@gmail.com",
     description="HTML file indexing generator.",
