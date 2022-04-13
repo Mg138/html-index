2f513f68afc7d4f8d0e2eb226c181545e418bc6a
Mg138 <muguang138@gmail.com>
2022-04-14 00:30:22 +0800

    fix

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="html-index",
-    version="1.0.8",
+    version="1.0.9",
     author="Madeline Sparkle",
     author_email="muguang138@gmail.com",
     description="HTML file indexing generator.",
